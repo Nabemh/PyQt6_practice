30df793eca72f0fc9a9b7cd061fbfdef10f82ebb
Nabemh <nhanior@gmail.com>
2023-07-01 10:52:31 +0100

    windows class created

---
+++ windowClass.py
@@ -0,0 +1,21 @@
+from PyQt6.QtWidgets import QApplication, QWidget
+import sys
+from PyQt6.QtGui import QIcon
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("PyQt6 Window")
+        self.setWindowIcon(QIcon("database-storage.png"))
+        #self.setFixedHeight(800)
+        #self.setFixedWidth(900)
+        
+        #this will take x and y coordinates
+        self.setGeometry(500,300, 400, 300)
+
+
+app = QApplication([])
+window = Window()
+window.show()
+sys.exit(app.exec())
